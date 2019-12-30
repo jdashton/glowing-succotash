28961c8f1b54e51b5df5c0a9b806e68b4df70536
J. Daniel Ashton <jdashton@ashtonfam.org>
2019-12-30 16:29:18 -0500

    AoC day 11 first steps

--- daniel/spec/advent11_spec.rb
+++ daniel/spec/advent11_spec.rb
@@ -1,17 +1,24 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'Advent11'
+require 'advent11'
 require 'tracer'
 
 describe Advent11 do
-  subject(:adv11) { described_class.new }
+  subject(:adv11) { described_class.new(inpipe, outpipe) }
+
+  let!(:inpipe) { IO.pipe }
+  let!(:outpipe) { IO.pipe }
 
   before do
     consts = {
-      'PRG1'  => [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]    
+      'PRG1' => [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8]
     }
     consts.each { |k, v| stub_const(k, v) }
   end
 
+  it 'outputs 0 as its first action' do
+    adv11.run
+    expect(outpipe[0].gets).to eq("0\n")
+  end
 end
