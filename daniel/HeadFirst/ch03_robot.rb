0e9833381ee55646c0440306c4e52d6c9a9cfe29
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-01-25 20:10:36 -0500

    HFR p99

---
+++ daniel/HeadFirst/ch03_robot.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# Robot
+class Robot
+  attr_accessor :name
+
+  def activate
+    puts "#{ @name } is powering up"
+  end
+
+  def move(destination)
+    puts "#{ @name } walks to #{ destination }"
+  end
+end
+
+# TankBot
+class TankBot < Robot
+  attr_accessor :weapon
+
+  def attack
+    puts "#{ @name } fires #{ @weapon }"
+  end
+
+  def move(destination)
+    puts "#{ @name } rolls to #{ destination }"
+  end
+end
+
+# SolarBot
+class SolarBot < Robot
+  def activate
+    puts "#{ @name } deploys solar panel"
+    super
+  end
+end
+
+tank = TankBot.new
+tank.name = 'Hugo'
+tank.weapon = 'laser'
+tank.activate
+tank.move('test dummy')
+tank.attack
+
+sunny = SolarBot.new
+sunny.name = 'Sunny'
+sunny.activate
+sunny.move('tanning bed')
