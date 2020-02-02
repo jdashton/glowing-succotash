3760cc905bb68323f3f2c7b9f6ed368ee879ce49
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-02-02 17:31:46 -0500

    HFR ch4 ex w keyword args

---
+++ daniel/HeadFirst/ch04_ex01.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+# Rectangle
+class Rectangle
+  attr_reader :width, :height
+
+  def width=(val)
+    raise 'width cannot be negative' if val.negative?
+
+    @width = val
+  end
+
+  def height=(val)
+    raise 'height cannot be negative' if val.negative?
+
+    @height = val
+  end
+
+  def initialize(width:, height:)
+    self.width = width
+    self.height = height
+  end
+
+  def area
+    width * height
+  end
+end
+
+rectangle = Rectangle.new(width: 2, height: 4)
+puts rectangle.area
