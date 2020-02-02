3b69aa6e392566af7e4911598edef4ad46aeec77
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-02-02 17:08:34 -0500

    HFR final code for ch 4

--- daniel/HeadFirst/ch04_employee.rb
+++ daniel/HeadFirst/ch04_employee.rb
@@ -10,12 +10,12 @@ class Employee
     @name = nom
   end
 
-  def print_name
-    puts format('Name: %<name>s', name: name)
-  end
-
   def initialize(name = 'Anonymous')
     self.name = name
+  end
+
+  def print_name
+    puts format('Name: %<name>s', name: name)
   end
 end
 
@@ -29,22 +29,33 @@ class SalariedEmployee < Employee
     @salary = sal
   end
 
+  def initialize(the_name = 'Anonymous', the_salary = 0.0)
+    super(the_name)
+    self.salary = the_salary
+  end
+
   def print_pay_stub
     print_name
     pay_for_period = (salary / 365.0) * 14
     formatted_pay = format('$%<pay>.2f', pay: pay_for_period)
     puts "Pay This Period: #{ formatted_pay }"
   end
-
-  def initialize(the_name = 'Anonymous', the_salary = 0.0)
-    # self.name = the_name
-    super(the_name)
-    self.salary = the_salary
-  end
 end
 
 # Hourly Employee
 class HourlyEmployee < Employee
+  def self.security_guard(name)
+    HourlyEmployee.new(name, 19.25, 30)
+  end
+
+  def self.cashier(name)
+    HourlyEmployee.new(name, 12.75, 25)
+  end
+
+  def self.janitor(name)
+    HourlyEmployee.new(name, 10.50, 20)
+  end
+
   attr_reader :hourly_wage, :hours_per_week
 
   def hourly_wage=(wage)
@@ -59,45 +70,24 @@ class HourlyEmployee < Employee
     @hours_per_week = hours
   end
 
-  def print_pay_stub
-    print_name
-    pay_for_period = hourly_wage * hours_per_week * 2
-    formatted_pay = format('$%<pay>.2f', pay: pay_for_period)
-    puts "Pay This Period: #{ formatted_pay }"
-  end
-
   def initialize(the_name = 'Anonymous', wage = 0.0, hours = 0.0)
     super(the_name)
     self.hourly_wage = wage
     self.hours_per_week = hours
   end
 
-  def self.security_guard(name)
-    HourlyEmployee.new(name, 19.25, 30)
-  end
-
-  def self.cashier(name)
-    HourlyEmployee.new(name, 12.75, 25)
-  end
-
-  def self.janitor(name)
-    HourlyEmployee.new(name, 10.50, 20)
+  def print_pay_stub
+    print_name
+    pay_for_period = hourly_wage * hours_per_week * 2
+    formatted_pay = format('$%<pay>.2f', pay: pay_for_period)
+    puts "Pay This Period: #{ formatted_pay }"
   end
 end
 
-SalariedEmployee.new('Amy Blake', 50_000).print_pay_stub
-
-ivan    = HourlyEmployee.cashier('Ivan Stokes')
-harold  = HourlyEmployee.cashier('Harold Nguyen')
-tamara  = HourlyEmployee.cashier('Tamara Wells')
-susie   = HourlyEmployee.cashier('Susie Powell')
-
-edwin   = HourlyEmployee.janitor('Edwin Burgess')
-ethel   = HourlyEmployee.janitor('Ethel Harris')
+jane = SalariedEmployee.new('Jane Doe', 50_000)
+jane.print_pay_stub
 
 angela  = HourlyEmployee.security_guard('Angela Matthews')
-stewart = HourlyEmployee.security_guard('Stewart Sanchez')
-
+ivan    = HourlyEmployee.cashier('Ivan Stokes')
 angela.print_pay_stub
-edwin.print_pay_stub
 ivan.print_pay_stub
