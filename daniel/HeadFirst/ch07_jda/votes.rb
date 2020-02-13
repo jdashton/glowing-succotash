7a0e066e6125070ad616e4de46f2e431f7b8283a
Daniel Ashton <jdashton@ashtonfam.org>
2020-02-13 05:02:37 -0500

    HFR p237

---
+++ daniel/HeadFirst/ch07_jda/votes.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+lines = []
+File.open('votes.txt') do |file|
+  lines = file.readlines
+end
+
+votes = Hash.new(0)
+
+lines.each do |line|
+  name = line.chomp
+  votes[name] += 1
+end
+
+pp votes
