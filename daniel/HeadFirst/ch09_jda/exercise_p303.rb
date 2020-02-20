5b3b03d4cf470b2ff98bb78d2e2d351500b24dea
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-02-19 19:56:33 -0500

    Test super with modules

--- daniel/HeadFirst/ch09_jda/exercise_p303.rb
+++ daniel/HeadFirst/ch09_jda/exercise_p303.rb
@@ -1,10 +1,19 @@
 module JetPropelled
+  def initialize
+    puts 'in JetPropelled#initialize'
+    super
+  end
+
   def move(destination)
     puts "Flying to #{ destination }."
   end
 end
 
 class Robot
+  def initialize
+    puts 'in Robot#initialize'
+  end
+
   def move(destination)
     puts "Walking to #{ destination }."
   end
@@ -13,12 +22,17 @@ end
 class TankBot < Robot
   include JetPropelled
 
+  def initialize
+    puts 'in TankBot#initialize'
+    super
+  end
+
   def move(destination)
     puts "Rolling to #{ destination }."
   end
 end
 
-class Hoverbot < Robot
+class HoverBot < Robot
   include JetPropelled
 end
 
@@ -28,3 +42,7 @@ end
 TankBot.new.move('hangar')
 HoverBot.new.move('lab')
 FarmerBot.new.move('field')
+
+p TankBot.ancestors
+p JetPropelled.ancestors
+p Robot.ancestors
