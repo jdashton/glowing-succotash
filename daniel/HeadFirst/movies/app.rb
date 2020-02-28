0f8f32cb08a851e8d88fd4ba06dcc2bc634917f1
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-02-28 15:33:16 -0500

    HFR ch15 p493 part 2

--- daniel/HeadFirst/movies/app.rb
+++ daniel/HeadFirst/movies/app.rb
@@ -1,17 +1,36 @@
 require 'sinatra'
+
 require 'movie'
+require 'movie_store'
+
+store = MovieStore.new('movies.yml')
 
 get('/movies') do
-  @movies = []
-  @movies[0] = Movie.new
-  @movies[0].title = "Jaws"
-  @movies[1] = Movie.new
-  @movies[1].title = "Alien"
-  @movies[2] = Movie.new
-  @movies[2].title = "Terminator 2"
+  @movies = store.all
   erb :index
 end
 
 get('/movies/new') do
   erb :new
+end
+
+helpers do
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
+end
+
+post('/movies/create') do
+  @movie = Movie.new
+  @movie.title = params['title']
+  @movie.director = params['director']
+  @movie.year = params['year']
+  store.save(@movie)
+  redirect '/movies/new'
+end
+
+get('/movies/:id') do
+  id = params['id'].to_i
+  @movie = store.find(id)
+  erb :show
 end
