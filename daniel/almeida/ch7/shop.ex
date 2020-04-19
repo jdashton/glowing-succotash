11bf875ba92289ff775fe842484bf108ca663f47
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-04-19 12:29:34 -0400

    Almeida p147

--- daniel/almeida/ch7/shop.ex
+++ daniel/almeida/ch7/shop.ex
@@ -1,13 +1,13 @@
 defmodule Shop do
   def checkout(price) do
-    quantity = ask_number("Quantity? ")
-    price = ask_number("Price? ")
-    calculate(quantity, price)
+    try do
+      {quantity, _} = ask_number("Quantity? ")
+      {price, _} = ask_number("Price? ")
+      quantity * price
+    rescue
+      MatchError -> "It's not a number"
+    end
   end
-
-  def calculate(:error, _), do: IO.puts("Quantity is not a number")
-  def calculate(_, :error), do: IO.puts("Price is not a number")
-  def calculate({quantity, _}, {price, _}), do: quantity * price
 
   def ask_number(message) do
     (message <> "\n")
