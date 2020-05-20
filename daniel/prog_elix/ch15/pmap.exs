cc99916b4276336f730e30b03d5d9f3a8bc7e1aa
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-05-20 17:17:38 -0400

    Thomas p211

---
+++ daniel/prog_elix/ch15/pmap.exs
@@ -0,0 +1,20 @@
+defmodule Parallel do
+  import :timer, only: [sleep: 1]
+
+  def pmap(collection, fun) do
+    me = self()
+
+    collection
+    |> Enum.map(fn elem ->
+      spawn_link(fn ->
+        sleep(Enum.random(1..1000))
+        send(me, {self(), fun.(elem)})
+      end)
+    end)
+    |> Enum.map(fn pid ->
+      receive do
+        {^pid, result} -> result
+      end
+    end)
+  end
+end
