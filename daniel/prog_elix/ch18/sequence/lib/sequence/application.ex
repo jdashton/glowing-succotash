f3e363bad10e9abb3206eb43fd9529a137017f7e
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-06-05 14:58:39 -0400

    Thomas ch 18

--- daniel/prog_elix/ch18/sequence/lib/sequence/application.ex
+++ daniel/prog_elix/ch18/sequence/lib/sequence/application.ex
@@ -1,19 +1,15 @@
 defmodule Sequence.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Sequence.Worker.start_link(arg)
-      # {Sequence.Worker, arg}
+      { Sequence.Stash,  123 },
+      { Sequence.Server, nil }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Sequence.Supervisor]
+    opts = [strategy: :rest_for_one, name: Sequence.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
