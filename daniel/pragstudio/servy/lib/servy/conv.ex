8995038e9b86f4986b72f0ca7169257ef10eba47
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-06-08 17:52:07 -0400

    PragStudio end of L19

--- daniel/pragstudio/servy/lib/servy/conv.ex
+++ daniel/pragstudio/servy/lib/servy/conv.ex
@@ -3,11 +3,24 @@ defmodule Servy.Conv do
             path: "",
             params: %{},
             headers: %{},
+            resp_headers: %{
+              "Content-Type" => "text/html",
+              "Content-Length" => 0
+            },
             resp_body: "",
             status: nil
 
+  def put_resp_content_type(conv, ctype) do
+    put_in(conv.resp_headers["Content-Type"], ctype)
+  end
+
+  def put_content_length(conv) do
+    headers = Map.put(conv.resp_headers, "Content-Length", String.length(conv.resp_body))
+    %{ conv | resp_headers: headers }
+  end
+
   def full_status(%Servy.Conv{} = conv) do
-    "#{ conv.status } #{ status_reason(conv.status) }"
+    "#{conv.status} #{status_reason(conv.status)}"
   end
 
   defp status_reason(code) do
--- daniel/pragstudio/servy/lib/servy/handler.ex
+++ daniel/pragstudio/servy/lib/servy/handler.ex
@@ -1,15 +1,15 @@
 defmodule Servy.Handler do
-
   @moduledoc "Handles HTTP requests."
 
   alias Servy.Conv
   alias Servy.BearController
 
-  @pages_path Path.expand("pages", File.cwd!)
+  @pages_path Path.expand("pages", File.cwd!())
 
   import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
   import Servy.Parser, only: [parse: 1]
   import Servy.FileHandler, only: [handle_file: 2]
+  import Servy.Conv, only: [put_content_length: 1]
 
   @doc "Transforms the request into a response."
   def handle(request) do
@@ -20,78 +20,99 @@ defmodule Servy.Handler do
     |> route
     |> track
     # |> emojify
+    |> put_content_length
     |> format_response
   end
 
-
-  def route(%Conv{ method: "GET", path: "/wildthings" } = conv) do
-    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
+  def route(%Conv{method: "GET", path: "/wildthings"} = conv) do
+    %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%Conv{ method: "GET", path: "/bears" } = conv) do
+  def route(%Conv{method: "GET", path: "/api/bears"} = conv) do
+    Servy.Api.BearController.index(conv)
+  end
+
+  def route(%Conv{method: "GET", path: "/bears"} = conv) do
     BearController.index(conv)
   end
 
   # name=Baloo&type=Brown
-  def route(%Conv{ method: "POST", path: "/bears" } = conv) do
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
     BearController.create(conv, conv.params)
   end
 
-  def route(%Conv{ method: "GET", path: "/bears/new" } = conv) do
+  def route(%Conv{method: "POST", path: "/api/bears"} = conv) do
+    Servy.Api.BearController.create(conv, conv.params)
+  end
+
+  def route(%Conv{method: "GET", path: "/bears/new"} = conv) do
     Path.expand("../../pages", __DIR__)
     |> Path.join("form.html")
-    |> File.read
+    |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/bears/" <> id } = conv) do
+  def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     params = Map.put(conv.params, "id", id)
     BearController.show(conv, params)
   end
 
-  def route(%Conv{ method: "GET", path: "/about" } = conv) do
+  def route(%Conv{method: "GET", path: "/about"} = conv) do
     @pages_path
     |> Path.join("about.html")
-    |> File.read
+    |> File.read()
     |> handle_file(conv)
   end
 
-  def route(%Conv{ method: "GET", path: "/pages/" <> file } = conv) do
+  def route(%Conv{method: "GET", path: "/pages/" <> file} = conv) do
     @pages_path
-    |> Path.join(file <> ".html")
-    |> File.read
+    |> Path.join(file <> ".md")
+    |> File.read()
     |> handle_file(conv)
+    |> markdown_to_html
   end
 
-  def route(%Conv{ method: "DELETE", path: "/bears/" <> _id } = conv) do
+  def route(%Conv{method: "DELETE", path: "/bears/" <> _id} = conv) do
     BearController.delete(conv, conv.params)
   end
 
-  def route(%Conv{ path: path } = conv) do
-    %{ conv | status: 404, resp_body: "No #{ path } here!"}
+  def route(%Conv{path: path} = conv) do
+    %{conv | status: 404, resp_body: "No #{path} here!"}
   end
 
+  def markdown_to_html(%Conv{status: 200} = conv) do
+    %{ conv | resp_body: Earmark.as_html!(conv.resp_body) }
+  end
 
-  def emojify(%Conv{ status: 200 } = conv) do
+  def markdown_to_html(%Conv{} = conv), do: conv
+
+  def emojify(%Conv{status: 200} = conv) do
     emojies = String.duplicate("🎉", 5)
     body = emojies <> "\n" <> conv.resp_body <> "\n" <> emojies
 
-    %{ conv | resp_body: body }
+    %{conv | resp_body: body}
   end
 
   def emojify(%Conv{} = conv), do: conv
 
   def format_response(%Conv{} = conv) do
     """
-    HTTP/1.1 #{ Conv.full_status(conv) }\r
-    Content-Type: text/html\r
-    Content-Length: #{ byte_size(conv.resp_body) }\r
+    HTTP/1.1 #{Conv.full_status(conv)}\r
+    #{format_response_headers(conv)}\r
     \r
-    #{ conv.resp_body }
+    #{conv.resp_body}
     """
   end
-end
 
+  def format_response_headers(conv) do
+    for {key, val} <- conv.resp_headers do
+      "#{key}: #{val}"
+    end
+    |> Enum.sort
+    |> Enum.reverse
+    |> Enum.join("\r\n")
+  end
+end
 
 # request = """
 # GET /wildlife HTTP/1.1
