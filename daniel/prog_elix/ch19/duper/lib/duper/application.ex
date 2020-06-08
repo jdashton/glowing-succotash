9861ce34e8fa62107184c4e49196cebefb0ebe10
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-06-08 14:30:17 -0400

    Thomas ch19

--- daniel/prog_elix/ch19/duper/lib/duper/application.ex
+++ daniel/prog_elix/ch19/duper/lib/duper/application.ex
@@ -1,19 +1,17 @@
 defmodule Duper.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Starts a worker by calling: Duper.Worker.start_link(arg)
-      # {Duper.Worker, arg}
+        Duper.Results,
+      { Duper.PathFinder, "/Users/jdashton/RubymineProjects" },
+        Duper.WorkerSupervisor,
+      { Duper.Gatherer,   16                                 }
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Duper.Supervisor]
+    opts = [strategy: :one_for_all, name: Duper.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
