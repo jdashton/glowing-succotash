be1023eeaed526a8acbdb1fd05dd7e8986397724
J. Daniel Ashton <jdashton@ashtonfam.org>
2020-06-16 17:33:46 -0400

    Thomas ch20 with Elixir releases

--- daniel/prog_elix/ch18/sequence/lib/sequence/server.ex
+++ daniel/prog_elix/ch18/sequence/lib/sequence/server.ex
@@ -1,5 +1,12 @@
 defmodule Sequence.Server do
   use GenServer
+  require Logger
+
+  @vsn "1"
+
+  defmodule State do
+    defstruct(current_number: 0, delta: 1)
+  end
 
   #####
   # External API
@@ -9,33 +16,43 @@ defmodule Sequence.Server do
   end
 
   def next_number do
-    GenServer.call __MODULE__, :next_number
+    with number = GenServer.call(__MODULE__, :next_number),
+      do: "The next number is #{number}"
   end
 
   def increment_number(delta) do
-    GenServer.cast __MODULE__, { :increment_number, delta }
+    GenServer.cast(__MODULE__, {:increment_number, delta})
   end
 
   #####
   # GenServer implementation
 
   def init(_) do
-    { :ok, Sequence.Stash.get() }
+    state = %State{current_number: Sequence.Stash.get()}
+    {:ok, state}
   end
 
-  def handle_call(:next_number, _from, current_number) do
-    { :reply, current_number, current_number + 1 }
+  def handle_call(:next_number, _from, state = %{current_number: n}) do
+    {:reply, n, %{state | current_number: n + state.delta}}
   end
 
-  def handle_cast({ :increment_number, delta }, current_number) do
-    { :noreply, current_number + delta }
-  end
-
-  def format_status(_reason, [ _pdict, state ]) do
-    [data: [{ 'State', "My current state is '#{inspect state}', and I'm happy" }]]
+  def handle_cast({:increment_number, delta}, state) do
+    {:noreply, %{state | delta: delta}}
   end
 
   def terminate(_reason, current_number) do
     Sequence.Stash.update(current_number)
+  end
+
+  def code_change("0", old_state = current_number, _extra) do
+    new_state = %State{
+      current_number: current_number,
+      delta: 1
+    }
+
+    Logger.info("Changing code from 0 to 1")
+    Logger.info(inspect(old_state))
+    Logger.info(inspect(new_state))
+    {:ok, new_state}
   end
 end
